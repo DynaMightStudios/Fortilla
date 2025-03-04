8a92f47ca2d9dcc858d1a5b63579d475bf2b351a
DynaMightStudios <excaliburpendragonii@gmail.com>
2025-03-04 09:44:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-#stage 1:
-#create plane, soldiers, firstpersoncontroller, fort, etc.
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+from fort import Fort
 
-#stage 2:
-#gameplay
+app = Ursina()
+
+
+# Initialize FirstPersonController
+player = FirstPersonController()
+player.speed = 15
+player.jump_height = 4
+player.height = 10
+
+crouching = False
+sprinting = False
+
+
+def update():
+    global crouching, sprinting
+    print(held_keys['c'])
+    if held_keys['c'] == 1:
+        player.speed = 8.9
+        crouching = True
+    else:
+        player.speed = 15
+        crouching = False
+
+plane = Entity(model='plane', texture='beach', scale=180, rotation = 0.2, collider='box')
+
+Fort_ = Fort
+Fort_.create()
+
+Sky(texture='Skyy')
+
+  
+app.run()
